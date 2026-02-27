4820311b55fcc6b140d83456d87b841e7471a1c7
Bejawada Mineesh <144539259+Mineesh-B@users.noreply.github.com>
2026-02-27 22:00:20 +0530

    files

---
+++ mul.py
@@ -0,0 +1,11 @@
+# def Multiplication(num):
+#     for i in range(1,11):
+#         print(f"{num}*{i}={num*i}")
+
+# Multiplication(5)
+
+def ran(a,b):
+    for i in range(a,b+1):
+        print(i,end=" ")
+
+ran(2,44)
